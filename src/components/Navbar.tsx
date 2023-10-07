a3e036b0f5f832f4960cd3cca20637fe6a12a418
SHUSHI <63992333+SHUSHI1137@users.noreply.github.com>
2023-10-07 11:15:43 +0700

    add tailwindcss

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <p>LearnHub</p>
+        <p>Login</p>
+        <p>Register</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
